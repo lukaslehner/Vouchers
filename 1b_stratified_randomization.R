91ef426f6edd67327397ec9dcde48db3e6c96319
AnnaMSchwarz <anna-magdalena.schwarz@wu.ac.at>
2021-02-06 15:13:02 +0100

    stratification

--- 1b_stratified_randomization.R
+++ 1b_stratified_randomization.R
@@ -22,20 +22,32 @@ wave_1$nationality_AUT_f<-as.factor(wave_1$nationality_AUT)
 wave_1$health_condition_f<-as.factor(wave_1$health_condition)
 wave_1$marginal_employment_f<-as.factor(wave_1$marginal_employment)
 wave_1$German_ok_f<-as.factor(wave_1$German_ok)
+wave_1$rgs_id_f<-as.factor(wave_1$rgs_id)
+wave_1$region_f<-as.factor(wave_1$region)
 
-wave_1$strata<-interaction(wave_1[,c("educ_f","agegr_f","male_f","nationality_AUT_f","health_condition_f","marginal_employment_f","German_ok_f")])
-#mit German_ok noch immer sehr viele Strata, ohne weniger(aber manche sehr klein)__> Lösung? 
+wave_1$strata<-interaction(wave_1[,c("educ_f","agegr_f","male_f","region_f")])
+summary(wave_1$strata)
+
+#no observations per strata
+wave_1<-wave_1 %>% 
+  group_by(strata) %>%
+  mutate(no_rows = length(personal_id))
+
 
 
 #------------------------------------------
+#set.seed()
 wave_1$group_nr<-block_ra(blocks=wave_1$strata, prob_each=c(rep(0.25,4)),num_arms=4)
 
 #check
 library(dplyr)
 library(expss)
 
-wave_1%>%tab_cells(educ_f,agegr_f,male_f,nationality_AUT_f,health_condition_f,marginal_employment_f,German_ok_f)%>%
+wave_1%>%tab_cells(educ_f,agegr_f,region_f,male_f,nationality_AUT_f,health_condition_f,marginal_employment_f,
+                   German_ok_f, ISCO08_1)%>%
   tab_cols(group_nr)%>%tab_stat_cpct()%>%tab_last_sig_cpct()%>%tab_pivot(stat_position = "outside_rows")
+
+
 
 
 # exporting files for AMS; one for each wave
