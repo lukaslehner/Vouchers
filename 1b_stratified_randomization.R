4d839d2e813cc7008b92b5e2c8cde977f7d79aa9
AnnaMSchwarz <anna-magdalena.schwarz@wu.ac.at>
2021-02-06 17:44:53 +0100

    stratification plus check plot and tables ready

--- 1b_stratified_randomization.R
+++ 1b_stratified_randomization.R
@@ -33,8 +33,6 @@ wave_1<-wave_1 %>%
   group_by(strata) %>%
   mutate(no_rows = length(personal_id))
 
-
-
 #------------------------------------------
 #set.seed()
 wave_1$group_nr<-block_ra(blocks=wave_1$strata, prob_each=c(rep(0.25,4)),num_arms=4)
@@ -42,13 +40,33 @@ wave_1$group_nr<-block_ra(blocks=wave_1$strata, prob_each=c(rep(0.25,4)),num_arm
 #check
 library(dplyr)
 library(expss)
+library(xtable)
 
-wave_1%>%tab_cells(educ_f,agegr_f,region_f,male_f,nationality_AUT_f,health_condition_f,marginal_employment_f,
-                   German_ok_f, ISCO08_1)%>%
+#strata sizes plot
+
+wave_1$strata = with(wave_1, reorder(strata, no_rows, median))
+wave_1 %>%
+  ggplot( aes(x=strata))+geom_bar()+labs(x="",y="observations",title = "Strata sizes")+
+  theme_minimal()+theme(axis.text.x=element_blank())
+
+#z-test between column percents each compared with each
+table11<-wave_1%>%tab_cells(educ_f,agegr_f,region_f,male_f,nationality_AUT_f,health_condition_f,marginal_employment_f,
+                   German_ok_f)%>%
   tab_cols(group_nr)%>%tab_stat_cpct()%>%tab_last_sig_cpct()%>%tab_pivot(stat_position = "outside_rows")
 
+print(xtable(table11,digits=1,include.colnames=FALSE,caption = "Treatment Balance"), caption.placement = 'top')
 
+#Chi-squared test of difference between the groups
+library(tableby)
+tab1 <- tableby(group_nr ~ male_f + agegr_f+ educ_f+region_f+nationality_AUT_f+health_condition_f+
+                  marginal_employment_f+German_ok_f, data=wave_1)
 
+capture.output(summary(tab1), file="Test.md")
+
+## Convert R Markdown Table to LaTeX
+require(knitr)
+require(rmarkdown)
+render("Test.md", pdf_document(keep_tex=TRUE))
 
 # exporting files for AMS; one for each wave
 nr = (1:4)
